9d39c8089e07c9f08643dd3ba119290166603f19
Ardi B <ardy1144malang@gmail.com>
2020-05-04 20:09:04 +0700

    cmmt

---
+++ app/src/main/java/com/example/boss/smkcoding1/profil.kt
@@ -0,0 +1,12 @@
+package com.example.boss.smkcoding1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class profil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
